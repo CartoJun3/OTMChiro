73e4b9b726f5d5740c22e6aaad468f32efe77df4
CartoJun3 <54879052+CartoJun3@users.noreply.github.com>
2025-02-04 22:32:36 +1000

    Fix base community page

---
+++ src/app/community/page.tsx
@@ -0,0 +1,9 @@
+export default function Community() {
+    return (
+        <div className="bg-white pt-24 sm:pt-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <p className="text-base/7 font-semibold text-indigo-600">OTM Chiropractic</p>
+        </div></div></div>
+    )
+}
