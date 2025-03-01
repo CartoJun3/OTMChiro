dfd47b0fb29fee8cb4db4dd5728951179d2f47df
CartoJun3 <54879052+CartoJun3@users.noreply.github.com>
2025-03-02 07:05:28 +1000

    fix title on booking

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -3,7 +3,7 @@ export default function Book() {
       <>
       <div className="text-center pt-32 mx-auto w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-3xl font-bold tracking-tighter text-neutral-800 sm:text-3xl">
-            <span className="sr-only">Make a Booking</span>Make a Booking
+            Make a Booking
           </h1>
       </div>
         <iframe id='cliniko-86827689' src='https://own-the-movement-chiropractic.au2.cliniko.com/bookings?embedded=true' scrolling='auto' width='100%' height='1000' style={{ pointerEvents: 'auto' }}></iframe> 
